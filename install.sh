b2d9930936dd503e8c5d1e7b6a3fb3817ca92d97
fecet <xiezej@gmail.com>
2025-08-21 23:04:53 +0800

    chore: install with mcp

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ pixi global install uv
 npm install -g @anthropic-ai/claude-code
 npm install -g ccstatusline
 npm install -g ccusage
+
+./mcp.sh
--- mcp.sh
+++ mcp.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+
 claude mcp add context7 --scope user -- npx -y @upstash/context7-mcp
 claude mcp add playwright npx @playwright/mcp@latest --scope user
