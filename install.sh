492d22694d3b2ee1f97cf48205910c627855e413
fecet <xiezej@gmail.com>
2025-08-30 14:52:35 +0800

    chore: add codex mcp

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 pixi global install pnpm
 pixi global install uv
+npm install -g bun
 npm install -g @anthropic-ai/claude-code
 npm install -g ccstatusline
 npm install -g ccusage
+npm install -g @openai/codex
 
 ./mcp.sh
 python CCPlugins/install.py
--- mcp.sh
+++ mcp.sh
@@ -4,7 +4,5 @@ set -x
 
 claude mcp add context7 --scope user -- npx -y @upstash/context7-mcp
 claude mcp add serena --scope user -- uv run --directory "$PIXI_PROJECT_ROOT/serena" serena start-mcp-server
-if [[ -n "$GITHUB_PAT" ]]; then
-  # claude mcp add --scope user --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer $(grep GITHUB_PAT .env | cut -d '=' -f2)"
-  claude mcp add --scope user --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer $(gh auth token)"
-fi
+claude mcp add --scope user --transport http github https://api.githubcopilot.com/mcp -H "Authorization: Bearer $(gh auth token)"
+claude mcp add codex --scope user -- codex --model gpt-5 --config model_reasoning_effort=high mcp
